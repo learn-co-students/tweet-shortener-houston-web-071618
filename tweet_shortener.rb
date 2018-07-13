f1f5e4f37147d4866f87978ec1bb658e938de36f
Richard Ojo <richardojo04@gmail.com>
2018-07-13 05:00:19 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def dictionary
+  words_to_sub = {  "hello" => 'hi'
+"to, two, too" => '2' 
+"for, four" => '4'
+'be' => 'b'
+'you' => 'u'
+"at" => "@" 
+"and" => "&" }
+end 
+
+def word_substituter(tweet)
+tweet_array = tweet.split(" ")
+tweet_array.map do |word|
+  replacements.each do |key, value|
+    word = value if word.downcase == key
+  end
+  word
+end.join(" ")
